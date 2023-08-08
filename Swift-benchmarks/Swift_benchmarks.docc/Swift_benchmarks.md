fda6429a8ffe6a77d22a8cfc639095c492dce716
Tom Ebergen <tom@ebergen.com>
2023-08-08 15:29:53 +0200

    Initial Commit

---
+++ Swift-benchmarks/Swift_benchmarks.docc/Swift_benchmarks.md
@@ -0,0 +1,13 @@
+# ``Swift_benchmarks``
+
+<!--@START_MENU_TOKEN@-->Summary<!--@END_MENU_TOKEN@-->
+
+## Overview
+
+<!--@START_MENU_TOKEN@-->Text<!--@END_MENU_TOKEN@-->
+
+## Topics
+
+### <!--@START_MENU_TOKEN@-->Group<!--@END_MENU_TOKEN@-->
+
+- <!--@START_MENU_TOKEN@-->``Symbol``<!--@END_MENU_TOKEN@-->
---
+++ Swift-benchmarks/Swift_benchmarks.h
@@ -0,0 +1,18 @@
+//
+//  Swift_benchmarks.h
+//  Swift-benchmarks
+//
+//  Created by Tom Ebergen on 08/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_benchmarks.
+FOUNDATION_EXPORT double Swift_benchmarksVersionNumber;
+
+//! Project version string for Swift_benchmarks.
+FOUNDATION_EXPORT const unsigned char Swift_benchmarksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_benchmarks/PublicHeader.h>
+
+
